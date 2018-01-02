587c19945e1ab5b9880264587cec9fb8de7476c6
zainmustafa <zmdhillo@gmail.com>
2018-01-02 23:05:15 +0500

    Fix email duplication issue in registration. This fixes #4

--- insert.php
+++ insert.php
@@ -10,11 +10,25 @@ $pin = $_POST["pin"];
 $email = $_POST["email"];
 $pwd = $_POST["pwd"];
 
-if($mysqli->query("INSERT INTO users (fname, lname, address, city, pin, email, password) VALUES('$fname', '$lname', '$address', '$city', $pin, '$email', '$pwd')")){
-	echo 'Data inserted';
-	echo '<br/>';
-}
-
-header ("location:login.php");
+	$sql="select * from users where (email='$email');";
+	$res=$mysqli->query($sql);
+	if (mysqli_num_rows($res) > 0) {
+		// output data of each row
+		$row = mysqli_fetch_assoc($res);
+		if($email==$row['email'])
+		{
+			echo "Email already exists";
+			header("Refresh: 3; url=register.php");
+		}
+	}
+	else { //here you need to add else condition
+		if($mysqli->query("INSERT INTO users (fname, lname, address, city, pin, email, password) VALUES('$fname', '$lname', '$address', '$city', $pin, '$email', '$pwd')")){
+			echo 'Registration Success!!';
+			header("Refresh: 3; url=login.php");
+		} else {
+			echo 'Registration Failure!!';
+			header ("Refresh: 3; url=register.php");
+		}
+	}
 
 ?>
